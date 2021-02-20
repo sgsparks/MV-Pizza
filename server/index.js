0774dfb544d2964079fb664dfa52a832e7a4775e
Jordan Weiner <weinerj8@gmail.com>
2021-02-19 22:34:18 -0800

    Build test endpoints for req.body, req.params, req.query workshop

--- server/index.js
+++ server/index.js
@@ -3,6 +3,22 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/api/cupcakes', (req, res) => {
+  console.log(req.body);
+  res.end();
+})
+
+app.post('/api/cookies/:flavor', (req, res) => {
+  console.log(req.params);
+  res.end();
+})
+
+app.post('/api/icecream/', (req, res) => {
+  console.log(req.query);
+  res.end();
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
