7090b477e0f24ba3fdc86cde9a1454155c5090b8
sam <sparks>
2021-03-30 10:18:42 -0700

    initail BP

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-const App = () => (
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans', sans-serif;
+
+  }
+
+  h1, h2, h3 {
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
+const App = () => {
+
+  return (
+    <>
+  <GlobalStyle />
+  <h1> MV-PIZZA </h1>
   <div>
     I am a react test
-  </div>
-);
+   </div>
+  </>
+  )
+  };
 
 export default App;
